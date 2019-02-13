c1c610986e5824c6cd565c9a969054998e958b17
Thành Lã <huuthanhla@gmail.com>
2019-02-13 15:13:37 +0700

    Edit currentcy

--- TTFortuneWheelSample/ViewController.swift
+++ TTFortuneWheelSample/ViewController.swift
@@ -10,18 +10,25 @@ import UIKit
 import TTFortuneWheel
 
 class ViewController: UIViewController {
+    
+    var got100 = false
+    var slices = [CarnivalWheelSlice]()
 
     @IBOutlet weak var spinningWheel: TTFortuneWheel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let slices = [ CarnivalWheelSlice.init(title: "  10$"),
-                       CarnivalWheelSlice.init(title: "  10$"),
-                       CarnivalWheelSlice.init(title: "  20$"),
-                       CarnivalWheelSlice.init(title: "  20$"),
-                       CarnivalWheelSlice.init(title: "  20$"),
-                       CarnivalWheelSlice.init(title: "  20$"),
-                       CarnivalWheelSlice.init(title: "  50$"),
-                       CarnivalWheelSlice.init(title: "  100$")]
+        slices = [ CarnivalWheelSlice.init(title: "  50$"),
+                   CarnivalWheelSlice.init(title: "  20$"),
+                   CarnivalWheelSlice.init(title: "  10$"),
+                   CarnivalWheelSlice.init(title: "  20$"),
+                   CarnivalWheelSlice.init(title: "  50$"),
+                   CarnivalWheelSlice.init(title: "  10$"),
+                   CarnivalWheelSlice.init(title: "  20$"),
+                   CarnivalWheelSlice.init(title: "  50$"),
+                   CarnivalWheelSlice.init(title: "  20$"),
+                   CarnivalWheelSlice.init(title: "  100$"),
+                   CarnivalWheelSlice.init(title: "  20$"),
+                   CarnivalWheelSlice.init(title: "  10$")]
         spinningWheel.slices = slices
         spinningWheel.equalSlices = true
         spinningWheel.frameStroke.width = 0
@@ -45,11 +52,23 @@ class ViewController: UIViewController {
     }
     
     @IBAction func rotateButton(_ sender: Any) {
+        
+        let count = spinningWheel.slices.count
 
         spinningWheel.startAnimating()
         DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
-            self.spinningWheel.startAnimating(fininshIndex: 5) { (finished) in
-                print(finished)
+            var finish = Int.random(in: 0..<count)
+            
+            if finish == 0, self.got100 {
+                finish = Int.random(in: 0..<count)
+            }
+            
+            self.spinningWheel.startAnimating(fininshIndex: finish) { (finished) in
+                if finish == 0 {
+                    self.got100 = true
+                }
+                print("\n\nfinish = \(finish)")
+                print(self.spinningWheel.slices[finish].title)
             }
         }
     }
